ea0b483c24626beb09361df50ad33af1c35c9ae8
Sakitoshi <39132563+Sakitoshi@users.noreply.github.com>
2026-02-19 18:03:41 -0300

    added atari 5200

---
+++ Emus/ATARI5200/launch_a5200.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+echo $0 $*
+RA_DIR=/mnt/SDCARD/RetroArch
+EMU_DIR=/mnt/SDCARD/Emus/ATARI5200
+cd $RA_DIR
+
+THIS_EMU=${0##*/}
+THIS_EMU=${THIS_EMU%.*}
+THIS_EMU=${THIS_EMU:7}
+ROM_DIR="$( cd -P -- "$(dirname "$1")" && pwd -P )"
+GAME_NAME="${1##*/}"
+GAME_NAME="${GAME_NAME%.*}"
+EMU_NAME=$(cat "${ROM_DIR}/${GAME_NAME}.emu")
+if [ -z "$EMU_NAME" ] || [ "$EMU_NAME" != "$THIS_EMU" ]; then
+  #echo $EMU_NAME > "${ROM_DIR}/${GAME_NAME}.emu"
+  # this is the default emu, let's remove the file instead
+  rm "${ROM_DIR}/${GAME_NAME}.emu"
+fi
+
+$EMU_DIR/cpufreq.sh
+
+HOME=$RA_DIR $RA_DIR/ra32.trimui $NET_PARAM -L $RA_DIR/.retroarch/cores/a5200_libretro.so "$*"
+sync
